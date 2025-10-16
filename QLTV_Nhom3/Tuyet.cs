44199e4cfa55c1c4054b5f454764c2717f279c6f
MYPC <MYPC@DESKTOP-K2AUNJ6>
2025-10-16 09:50:01 +0700

    Đây là bài của Tuyết

---
+++ QLTV_Nhom3/Tuyet.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace QLTV_Nhom3
+{
+    public partial class Tuyet : Form
+    {
+        public Tuyet()
+        {
+            InitializeComponent();
+        }
+    }
+}
